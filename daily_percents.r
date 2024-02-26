7bff185adbefc0ca72d4dfa137d42d2b869158cf
Patrick Woodworth <p77wood@gmail.com>
2024-02-26 16:45:22 -0600

    Percent change per day. Calc wrong, logic right.

--- daily_percents.r
+++ daily_percents.r
@@ -4,11 +4,13 @@ input_data_list <- list.files(path = "historical_data/csv_data", full.names = TR
 for (file in input_data_list) {
     input_data_frame <- ldply( .data = file,
                     .fun = read.csv,
-                    header = FALSE,
+                    header = TRUE,
                     col.names=c("Index","Date","Close","Volume","Open","High","Low") )
+    input_data_frame[-1,]
+
     daily_close <- input_data_frame[3]
     daily_open <- input_data_frame[5]
     percent_change <- rep(NA, length(input_data_frame))
-    percent_change <- daily_close / daily_open
+    percent_change <- daily_close/daily_open
 
 }
