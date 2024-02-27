f62d6b9e721540743940221cf08406a6eaaa79df
Woodworth <patrick.woodworth@capgemini.com>
2024-02-27 11:59:49 -0600

    Broke code into functions

--- parse_historical_stock_data.r
+++ parse_historical_stock_data.r
@@ -1,5 +1,29 @@
 library(jsonlite)
 
+correct_datatypes <- function(framed_historical_data) {
+    framed_historical_data$Close <- as.numeric(framed_historical_data$Close)
+    framed_historical_data$Open <- as.numeric(framed_historical_data$Open)
+    framed_historical_data$High <- as.numeric(framed_historical_data$High)
+    framed_historical_data$Low <- as.numeric(framed_historical_data$Low)
+
+    framed_historical_data$Date <- gsub("/", "", framed_historical_data$Date)
+    framed_historical_data$Date <- as.Date(framed_historical_data$Date, "%m%d%Y")
+
+    return(framed_historical_data)
+}
+
+add_daily_percents <- function(framed_historical_data){
+    daily_close <- framed_historical_data$Close
+    daily_open <- framed_historical_data$Open
+    
+    percent_change <- rep(NA, length(framed_historical_data))
+    percent_change <- daily_close/daily_open
+
+    framed_historical_data$Percent <- percent_change
+
+    return(framed_historical_data)
+}
+
 input_data_list <- list.files(path = "historical_data/json_data", full.names = TRUE)
 for (file in input_data_list) {
     historical_data <- fromJSON(file)
@@ -27,21 +51,8 @@ for (file in input_data_list) {
 
     colnames(framed_historical_data) <- c("Date", "Close", "Volume", "Open", "High", "Low")
 
-    framed_historical_data$Close <- as.numeric(framed_historical_data$Close)
-    framed_historical_data$Open <- as.numeric(framed_historical_data$Open)
-    framed_historical_data$High <- as.numeric(framed_historical_data$High)
-    framed_historical_data$Low <- as.numeric(framed_historical_data$Low)
-
-    framed_historical_data$Date <- gsub("/", "", framed_historical_data$Date)
-    framed_historical_data$Date <- as.Date(framed_historical_data$Date, "%m%d%Y")
-
-    daily_close <- framed_historical_data$Close
-    daily_open <- framed_historical_data$Open
-    
-    percent_change <- rep(NA, length(framed_historical_data))
-    percent_change <- daily_close/daily_open
-
-    framed_historical_data$Percent <- percent_change
+    framed_historical_data <- correct_datatypes(framed_historical_data)
+    framed_historical_data <- add_daily_percents(framed_historical_data)
 
     is_num <- sapply(framed_historical_data, is.numeric)
     framed_historical_data[is_num] <- lapply(framed_historical_data[is_num], round, 4)
@@ -54,4 +65,6 @@ for (file in input_data_list) {
 
     plot(y = framed_historical_data$Close, x = framed_historical_data$Date, xlab = "Date", ylab = "Close Price", type = "b")
 }
+
+
 
