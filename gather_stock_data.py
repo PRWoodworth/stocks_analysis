b3e41b43a4ae9e1a6ee55ba22e794591a80cd806
Patrick Woodworth <p77wood@gmail.com>
2024-02-29 15:57:02 -0600

    Corrected end scenario. Corrected timer.

--- gather_stock_data.py
+++ gather_stock_data.py
@@ -9,9 +9,9 @@ import time
 
 data = []
 symbol = []
-num_threads = 10
+num_threads = 20
 
-def query_stock_data(stock_ticker_list):
+def query_stock_data(stock_ticker_list, task_number):
     to_date = date.today()
     from_date = to_date - relativedelta(years=10)
     str_to_date = to_date.strftime('%Y-%m-%d')
@@ -23,9 +23,10 @@ def query_stock_data(stock_ticker_list):
         tickerName = stock_ticker_list.get()
         if tickerName is None:
             next()
-        print(tickerName)
-        z = z + 1
-        print(z)
+        print("Worker task",task_number,"beginning on ticker", tickerName)
+        work_start = time.time()
+        # z = z + 1
+        # print(z)
         url = "https://api.nasdaq.com/api/quote/" + tickerName + "/historical?assetclass=stocks&fromdate=" + str_from_date + "&limit=2517&todate=" + str_to_date
         information = json.loads(s.get(url, headers=headers, timeout=5).content)
         filename = "historical_data/json_data/" + tickerName + ".json"
@@ -34,6 +35,9 @@ def query_stock_data(stock_ticker_list):
             outfile.write(json_data)
             outfile.close
         stock_ticker_list.task_done()
+        work_done = time.time()
+        work_total = work_done - work_start
+        print("Worker task",task_number,"completed on ticker", tickerName, "after", work_total, "seconds.")
         s.close()
 
 
@@ -50,15 +54,14 @@ def main():
     stock_ticker_list = read_stock_tickers()
     stock_ticker_queue = Queue()
     for i in range(num_threads): 
-        threading.Thread(target = query_stock_data, args = (stock_ticker_queue,)).start()
-
-    stock_ticker_queue.join()
+        threading.Thread(target = query_stock_data, args = (stock_ticker_queue,i,)).start()
 
     for x in stock_ticker_list:
         stock_ticker_queue.put(x)
 
+    stock_ticker_queue.join()
     end = time.time()
-    duration = start - end 
+    duration = end - start 
     print("All stock tickers queried in",duration,"seconds.")
 
 if __name__ == '__main__':
