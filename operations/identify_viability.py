c27ca8c9f82fa185e60fbf6e3e5c8285be692f09
Woodworth <patrick.woodworth@capgemini.com>
2024-03-06 12:03:28 -0600

    File target changes again

--- operations/identify_viability.py
+++ operations/identify_viability.py
@@ -11,20 +11,22 @@ import logging
 import os 
 import sys
 import pandas as pd 
+import glob
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 log_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'logs')
 log_fname = os.path.join(log_dir, 'identify_viability.log')
-csv_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'historical_data/csv_data')
+csv_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'historical_data\\csv_data')
 
 logging.basicConfig(filename=log_fname, encoding='utf-8', level=logging.DEBUG, filemode = "w")
 
 def iterate_pull_data():
     data_frame = pd.DataFrame() 
-    csv_files = os.listdir(csv_dir)
+    csv_files = glob.glob(csv_dir + '\\*')
     for file in csv_files:
         logging.info("Starting baseline viability check on %s", file)
         df = pd.read_csv(file) 
+        # TODO: change to .concat() instead of .append()
         data_frame.append(df) 
         viability = check_viability(data_frame, 30)
         print_viability(viability, data_frame)
@@ -32,7 +34,10 @@ def iterate_pull_data():
 
 def check_viability(input_data_frame, time_period):
     viability = 0
-    viability = input_data_frame['Percent'].head(time_period).sum() / time_period
+    percent_column = input_data_frame[["Percent"]]
+    percent_column_time_period_data = percent_column.head(time_period)
+    percent_column_sum = percent_column_time_period_data.sum()
+    viability = percent_column_sum / time_period
     return viability
 
 # TODO: generate CSV file of identified viable tickers
