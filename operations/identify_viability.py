07ae427927dcee2854ce555181dbfa7dc4dc1520
Woodworth <patrick.woodworth@capgemini.com>
2024-03-12 11:45:22 -0500

    CSV file output indicates num days

--- operations/identify_viability.py
+++ operations/identify_viability.py
@@ -9,7 +9,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 log_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'logs')
 log_fname = os.path.join(log_dir, 'identify_viability.log')
 csv_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'historical_data\\csv_data')
-viability_fname = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'historical_data\\viability.csv')
+
 
 logging.basicConfig(filename=log_fname, encoding='utf-8', level=logging.DEBUG, filemode = "w")
 
@@ -21,7 +21,7 @@ def percent_to_float(x):
         logging.exception("Unable to de-percent input: %s", x)
     
 
-def iterate_pull_data():
+def iterate_pull_data(timeframe_days):
     data_frame = pd.DataFrame() 
     csv_files = glob.glob(csv_dir + '\\*')
     viability_frame = pd.DataFrame(columns = ['Ticker', 'Percent'])
@@ -31,7 +31,7 @@ def iterate_pull_data():
         logging.info("Starting baseline viability check on %s", filename)
         data_frame = pd.read_csv(file, converters={'Percent':percent_to_float}, header=0)
         data_frame = data_frame.loc[:, ~data_frame.columns.str.contains('^Unnamed')]
-        viability = float(check_viability(data_frame, 7))
+        viability = float(check_viability(data_frame, timeframe_days))
         logging.info("Baseline viability: %s", viability)
         viability_frame = pd.concat([pd.DataFrame([[ticker_name, viability]], columns = viability_frame.columns), viability_frame], ignore_index = True)
         data_frame.loc[:] = None
@@ -46,13 +46,15 @@ def check_viability(input_data_frame, time_period):
     viability = percent_column_sum / time_period
     return viability
 
-def print_viability(data_frame):
+def print_viability(data_frame, timeframe_days):
+    viability_fname = os.path.join(os.path.normpath(os.getcwd() + os.sep), ('historical_data\\viability_days_%s.csv' % timeframe_days))
     data_frame.to_csv(viability_fname ,encoding='utf-8')
     return
 
 def main():
-    viability_frame = iterate_pull_data()
-    print_viability(viability_frame)
+    timeframe_days = 7
+    viability_frame = iterate_pull_data(timeframe_days)
+    print_viability(viability_frame, timeframe_days)
     sys.exit()
 
 if __name__ == '__main__':
