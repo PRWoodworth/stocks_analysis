bb30c4e4d1dbb383e794c1605eb9715abd8cad64
Patrick Woodworth <p77wood@gmail.com>
2024-04-10 21:30:48 -0500

    Troubleshooting logging

--- operations/graphing.py
+++ operations/graphing.py
@@ -12,15 +12,18 @@ import plotly.express as px
 dir_path = os.path.dirname(os.path.realpath(__file__))
 log_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'logs')
 graphing_log_fname = os.path.join(log_dir, 'graphing.log')
-csv_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'historical_data\\csv_data\\monthly_averages')
+csv_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'historical_data\\csv_data\\monthly_averages\\')
 
 
 logging.basicConfig(filename=graphing_log_fname, encoding='utf-8', level=logging.DEBUG, filemode = "w")
 
 def generate_graph():
     ticker_to_graph = request.get_json().get('ticker')
+    logging.info("TARGET TICKER: %s" %ticker_to_graph)
     target_file_path = csv_dir+ticker_to_graph+'_monthly_average.csv'
+    logging.info("TARGET FILE PATH: %s" %target_file_path)
     target_file = glob.glob(target_file_path, recursive=False)
+    logging.info("TARGET FILE: %s" %target_file)
     with open(target_file[0]) as file:
         avg_frame = pd.read_csv(file, header=0)
         
