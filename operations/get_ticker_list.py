0054a944f4103d213bdb8cbc47a57d3bdba9b39b
Woodworth <patrick.woodworth@capgemini.com>
2024-05-07 11:27:27 -0500

    Corrected value

--- operations/get_ticker_list.py
+++ operations/get_ticker_list.py
@@ -8,7 +8,7 @@ from flask import json
 dir_path = os.path.dirname(os.path.realpath(__file__))
 log_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'logs')
 ticker_list_log_name = os.path.join(log_dir, 'ticker_list.log')
-csv_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'historical_data\\csv_data\\raw_data')
+csv_dir = os.path.join(os.path.normpath(os.getcwd() + os.sep), 'historical_data\\csv_data\\monthly_averages')
 
 logging.basicConfig(filename=ticker_list_log_name, encoding='utf-8', level=logging.DEBUG, filemode = "w")
 
@@ -17,7 +17,7 @@ def get_ticker_list():
     csv_files = [file for file in glob.glob(csv_dir + '\\*', recursive=False) if not os.path.isdir(file)]
     
     for file in csv_files:
-        separator = 'raw_data\\'
+        separator = 'monthly_averages\\'
         target_file = file.split(separator, 1)[1]
         separator = '.csv'
         target_file = target_file.split(separator, 1)[0]
